ebf05eb019b6b6c0f745b86c245b0588964f3b04
Jacky Ka Long Ko <oyk357@cluster2.hpc.in.bmrc.ox.ac.uk>
2025-03-25 15:16:35 +0000

    auto relative path in slurm env

--- tracking/segment.sh
+++ tracking/segment.sh
@@ -19,9 +19,9 @@ mamba activate cyto
 #     -b $SLURM_ARRAY_TASK_ID -r napari-ome-zarr -el edge -dl detection
 
 # to accomodate pyCyto workflow now it need to provide absolute path of the script, this will not take script from SLURM working sandbox, possible causing bug if you change code after submission, otherwise use the relative path:
-# SCRIPT_DIR=$(dirname "$(realpath "$0")")
-SCRIPT_DIR="/gpfs3/well/kir-fritzsche/projects/Cytotoxicity-Pipeline-Distributed/dependency/Ultrack-Cluster/tracking"
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+# SCRIPT_DIR="/gpfs3/well/kir-fritzsche/projects/Cytotoxicity-Pipeline-Distributed/dependency/Ultrack-Cluster/tracking"
 
 # binning will automatically take care of length of data, for specfic time range edit in main.sh
 # reserver length for reference
-python $SCRIPT_DIR/segment.py -p "$1" --cfg "$2" -b "$3" -e "$4" -bi $SLURM_ARRAY_TASK_ID -bp 3
+python $SCRIPT_DIR/dependency/Ultrack-Cluster/tracking/segment.py -p "$1" --cfg "$2" -b "$3" -e "$4" -bi $SLURM_ARRAY_TASK_ID -bp 3
